53970c0f7c6309b58d69802fef9f7038dfc4585a
BalazsMikola <mbalazs86@gmail.com>
2019-06-18 10:40:49 +0200

    a7-2

--- cheer.js
+++ cheer.js
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 function cheer(parameter) {
   console.log(parameter);
 }
 
 cheer('Cheer')
-=======
-function cheer(param1,param2) {
-  console.log(param1,param2);
-}
-
-cheer("egy", "ketto");
->>>>>>> master
